efbe953325dbf4b40ff4fef2f508dd077da9d550
ronesta <gvozzzd123@gmail.com>
2024-11-16 19:34:06 +0300

    Initial commit

---
+++ iTunesDiskStorage/iTunesDiskStorage/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iTunesDiskStorage
+//
+//  Created by Ибрагим Габибли on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
